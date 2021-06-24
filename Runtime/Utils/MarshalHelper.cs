f27255b0b3dd52bccfcb5fbbbddad4fa6f725177
Georgy Ushakov <gush@equinor.com>
2021-06-24 09:37:51 +0200

    Allow empty meshes

--- Runtime/Utils/MarshalHelper.cs
+++ Runtime/Utils/MarshalHelper.cs
@@ -7,6 +7,8 @@ namespace FastObjUnity.Utils
     {
         internal static float[] RetrieveFloats(IntPtr floatArray, int length)
         {
+            if (length == 0)
+                return Array.Empty<float>();
             var toReturn = new float[length];
             Marshal.Copy(floatArray, toReturn, 0, length);
             return toReturn;
@@ -14,6 +16,8 @@ namespace FastObjUnity.Utils
 
         internal static int[] RetrieveInts(IntPtr intArray, int length)
         {
+            if (length == 0)
+                return Array.Empty<int>();
             var toReturn = new int[length];
             Marshal.Copy(intArray, toReturn, 0, length);
             return toReturn;
@@ -21,6 +25,8 @@ namespace FastObjUnity.Utils
         
         internal static T[] RetrieveStructs<T>(IntPtr structArrayPtr, int length) where T:struct
         {
+            if (length == 0)
+                return Array.Empty<T>();
             var result = new T[length];
             var pos = structArrayPtr;
             var step = Marshal.SizeOf<T>();
