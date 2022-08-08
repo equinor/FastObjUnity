661f318375d35ba5f8154e192d1f924a060d2fc2
Eirik Wilhelmsen <ewilh@equinor.com>
2022-08-08 11:43:46 +0200

    Cleaned code and fixed PR comments

--- Editor/FastObjImporter.cs
+++ Editor/FastObjImporter.cs
@@ -1,15 +1,13 @@
 ﻿using FastObjUnity.Runtime;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using UnityEditor.AssetImporters;
+using UnityEngine;
 
 namespace FastObjUnity.Editor
 {
-    using System.Collections.Generic;
-    using System.Diagnostics;
-    using System.IO;
-    using UnityEditor.AssetImporters;
-    using UnityEngine;
-
-    // Unity does not allow us to override the default importer, so we have to unfortunately use something else, than
-    // obj.
+    // Unity does not allow us to override the default importer, so we have to unfortunately use another file ending than obj.
     [ScriptedImporter(1, "obj_fast", AllowCaching = true)]
     public class FastObjImporter : ScriptedImporter
     {
@@ -19,21 +17,21 @@ namespace FastObjUnity.Editor
             var nameSet = new HashSet<string>();
             var material = new Material(Shader.Find("Standard"));
             var globalPath = Path.Combine(Application.dataPath, "..", ctx.assetPath);
-            var meshes = FastObjConverter.TestFastObj(globalPath);
+            var meshes = FastObjConverter.ImportFastObj(globalPath);
             var gameObjectName = Path.GetFileName(globalPath);
             var model = new GameObject(gameObjectName);
             var modelTransform = model.transform;
+
             ctx.AddObjectToAsset(GetFreeNameIdentifier(gameObjectName, nameSet), model);
             foreach (var child in meshes)
                 CreateNode(child, modelTransform, ctx, nameSet, material);
             ctx.AddObjectToAsset(GetFreeNameIdentifier("defaultMaterial", nameSet), material);
             ctx.SetMainObject(model);
             stopwatch.Stop();
-            UnityEngine.Debug.Log($"Import of {ctx.assetPath} completed in {stopwatch.Elapsed}");
+            UnityEngine.Debug.Log($"Import of {ctx.assetPath} completed in {(stopwatch.ElapsedMilliseconds / 1000f):F1} seconds");
         }
 
-        private void CreateNode((string Key, Mesh Value) meshWithName, Transform parent, AssetImportContext ctx,
-            HashSet<string> nameSet, Material material)
+        private void CreateNode((string Key, Mesh Value) meshWithName, Transform parent, AssetImportContext ctx, HashSet<string> nameSet, Material material)
         {
             var nodeName = string.IsNullOrWhiteSpace(meshWithName.Key) ? "unnamed" : meshWithName.Key;
             var go = new GameObject(nodeName);
